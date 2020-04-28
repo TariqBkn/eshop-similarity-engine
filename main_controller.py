d133a6da9a7659e5d97bc79da8ac3f7d7ffa0567
TariqBkn <boukouyen.tariq@gmail.com>
2020-04-28 08:04:44 +0000

    Create endpoint /products/<int:productId>/similar

--- main_controller.py
+++ main_controller.py
@@ -1,5 +1,17 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+import similarity_engine
 
 app = Flask(__name__)
 api = Api(app)
+
+class Similarity(Resource):
+    def __init__(self):
+        self.se = similarity_engine.Similarity_engine()
+    def get(self, productId):
+        return self.se.get_similar_products_of(productId)
+
+api.add_resource(Similarity,'/products/<int:productId>/similar')
+
+if __name__=='__main__':
+    app.run(debug=True)
